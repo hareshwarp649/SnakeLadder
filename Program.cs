1f678bb4b550555cccf7c1d147102b26602a9578
Harry <hareshwarp649>
2022-02-22 21:08:26 +0530

    New Message

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Snake Ladder Problem");
+            //SnakeGame.GameSnake();
+            SnakeLadderGame.GameSnakeLadder();
+        }
+    }
+}
